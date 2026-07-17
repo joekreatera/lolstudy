64cf66d45ea43999d8466adfd7bede05b4cf7523
Joe Mondragon <jose.c.mondragon@gmail.com>
2026-07-17 07:51:51 -0600

    Create postgres_db.py
    base, careful with settings

---
+++ postgres_db.py
@@ -0,0 +1,20 @@
+from databases import Database
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Float
+
+from ..settings import DB_URL
+
+
+database = Database(DB_URL)
+metadata = MetaData()
+
+books = Table (
+  "books",
+  metadata,
+  Column("id", Integer, primary_key=True, index=True),
+  Column("title", String, index=True),
+  Column("author", String, index=True),
+  Column("price", Float),
+)
+
+engine = create_engine(DB_URL)
+metadata.create_all(engine)
